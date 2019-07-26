cae14da384ba7d06274abceeae753db2d8b6a2b4
brianwu <webb.brian.wu@163.com>
2019-07-26 16:28:33 +0800

    第二次编写！

--- CAGitStart/CAGitStart/Program.cs
+++ CAGitStart/CAGitStart/Program.cs
@@ -11,7 +11,7 @@ namespace CAGitStart
         static void Main(string[] args)
         {
             Console.WriteLine("Start work,Hello world!");
-
+            Console.WriteLine("2end write code！");
         }
     }
 }
